e7a5ccfa46b30b34ce5f30fe202596a1cc54f7cb
robocop0001 <s.chauhan.p2.27@gmail.com>
2026-07-20 20:38:54 +1000

    added serializers file for my backend model

---
+++ store/serializers.py
@@ -0,0 +1,32 @@
+"""
+| Serializers weren't automatically generated with the boilerplate I activated
+| for this application. I made this.
+| I also used nested serialisers to handle foreign keys as per this Stack
+| Overflow thread:
+| https://stackoverflow.com/questions/75386610/how-to-serialize-django-model-with-2-or-more-foreign-keys
+"""
+
+from rest_framework import serializers
+from .models import Store, Address, Item
+
+class StoreSerializer(serializers.Serializer):
+    class Meta:
+        model = Store
+        fields = '__all__'
+        read_only_fields = '__all__'
+
+class AddressSerializer(serializers.Serializer):
+    store = StoreSerializer()
+
+    class Meta:
+        model = Address
+        fields = '__all__'
+        read_only_fields = ['store']
+
+class ItemSerializer(serializers.Serializer):
+    store = StoreSerializer()
+
+    class Meta:
+        model = Item
+        fields = '__all__'
+        read_only_fields = ['name']
